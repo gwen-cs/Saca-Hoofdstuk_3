9bbe8c7692fc3ffbff35834309d2b87dc3cdb631
Gwen Reynders <gwenneke@pm.me>
2026-08-14 22:08:15 +0200

    opstart theorie

---
+++ main.py
@@ -0,0 +1,39 @@
+
+# dit is commentaar
+"""
+boodschap = "Hello-Wold"
+print(boodschap)
+
+name = input("What's your name?: ")
+print ("hello", end=" ")
+print (name)
+
+print('hello, "friend"')
+
+name = input("What's your name?: ")
+print (f"your name is, {name} ")
+
+name = input("What's your name?: ")
+name = name.strip()
+print(f"Hello {name}")
+name = name.title()
+print(f"Hello {name}")
+
+name = input("What's your name?: ")
+name = name.strip().title()
+print(f"Hello {name}")
+
+name = input("What's your name?: ").strip().title()
+print(f"Hello {name}")
+
+x = int(input("what's x?: "))
+y = int(input("What's y?: "))
+z = x + y
+print (z)
+
+
+"""
+x = int(input("what's x?: "))
+y = int(input("What's y?: "))
+z = x + y
+print (z)
