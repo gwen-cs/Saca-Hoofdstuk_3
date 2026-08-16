e71fc392c2ebc440e2819b8516c66fbb4ea71344
Gwen Reynders <gwenneke@pm.me>
2026-08-16 11:42:59 +0200

    klaar met deze oefening

--- 1-2ToDo.py
+++ 1-2ToDo.py
@@ -5,11 +5,49 @@ agenda_punten = {
     9   :   "exit"
 }
 
+def add_todo():
+    item = input("Geef item dat je nog moet doen ?: ")
+
+    todo_list.append(item)
+
+
+
+def remove_todo():
+    number = int(input("Welk item wil je dat ik wis?").strip())
+
+    for i, do in enumerate (todo_list):
+        if (i == number) :
+            todo_list.remove(do)
+            break
+
+def list_todo():
+    for i, do in enumerate (todo_list):
+        print(f"{i} - {do}")
+
+
 for index, agenda in agenda_punten.items():
     print(f"{index} - {agenda}")
 
 state = int(input("Enter a number?: "))
 
+todo_list = ["fietsen", "vissen"]
 
 while state != 9:
+
+    if (state == 1):
+        add_todo()
+
+    if (state == 2):
+        remove_todo ()
+
+    if (state == 3):
+        list_todo()
+
     state = int(input("Enter a number?: "))
+
+
+
+
+
+
+
